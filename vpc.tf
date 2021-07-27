ba73efa12856a4d8b7c28e3c47f2d375c3ead6ea
inductor <kohei.ota@hpe.com>
2021-07-27 18:05:58 +0900

    VPC setup

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.100.0.0/16"
+
+  azs             = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  private_subnets = ["10.100.1.0/24", "10.100.2.0/24", "10.100.3.0/24"]
+  public_subnets  = ["10.100.101.0/24", "10.100.102.0/24", "10.100.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
