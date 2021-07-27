36ba1bc4c2181dd4cf9f1d852ed1c214f4f52838
inductor <kohei.ota@hpe.com>
2021-07-27 18:32:14 +0900

    add db subnet

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,11 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "vpc"
   cidr = "10.100.0.0/16"
 
   azs             = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  database_subnets    = ["10.100.201.0/24", "10.100.202.0/24", "10.100.203.0/24"]
   private_subnets = ["10.100.1.0/24", "10.100.2.0/24", "10.100.3.0/24"]
   public_subnets  = ["10.100.101.0/24", "10.100.102.0/24", "10.100.103.0/24"]
 
